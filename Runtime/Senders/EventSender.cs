ae0b6524d135b7e60a07bbf28eb32736422e1dfc
Jean-Francois Robin <jean-francois_robin@uqar.ca>
2023-04-18 16:19:06 -0400

    sender update

--- Runtime/Senders/EventSender.cs
+++ Runtime/Senders/EventSender.cs
@@ -3,12 +3,8 @@ using UnityEngine.Serialization;
 
 namespace jeanf.EventSystem
 {
-    public class EventSender : MonoBehaviour
+    public interface EventSender
     {
-        [Header("Broadcasting on channels")]
-        public StringEventChannelSO _stringMessageChannel;
-        public BoolEventChannelSO _boolMessageChannel;
-        public IntEventChannelSO _intMessageChannel;
-        public VoidEventChannelSO _voidMessageChannel;
+        StringEventChannelSO stringMessageChannel { get; set; }
     }
 }
