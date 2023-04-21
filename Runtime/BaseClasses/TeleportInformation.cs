4e599500dc122aaff4d0ba8f5208740fa94138fe
Jean-Francois Robin <jean-francois_robin@uqar.ca>
2023-04-20 20:57:03 -0400

    added option not to use the filters

--- Runtime/BaseClasses/TeleportInformation.cs
+++ Runtime/BaseClasses/TeleportInformation.cs
@@ -11,6 +11,7 @@ namespace jeanf.EventSystem
         public Transform objectToTeleport;
         public Transform targetDestination;
         public bool alignToTarget;
+        public bool isUsingFilter;
         public FilterSO filter;
         
         public TeleportInformation(Transform targetDestination, bool alignToTarget)
@@ -20,13 +21,14 @@ namespace jeanf.EventSystem
             this.objectIsPlayer = true;
         }
 		
-        public TeleportInformation(Transform objectToTeleport, Transform targetDestination, bool alignToTarget, bool objectIsPlayer, FilterSO filter)
+        public TeleportInformation(Transform objectToTeleport, Transform targetDestination, bool alignToTarget, bool objectIsPlayer, FilterSO filter, bool isUsingFilter)
         {
             this.objectToTeleport = objectToTeleport;
             this.targetDestination = targetDestination;
             this.alignToTarget = alignToTarget;
             this.objectIsPlayer = objectIsPlayer;
             this.filter = filter;
+            this.isUsingFilter = isUsingFilter;
         }
     }
 }
