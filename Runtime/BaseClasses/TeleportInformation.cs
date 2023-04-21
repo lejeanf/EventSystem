53ca930f4317c0b850be3dde42e4a42a404025f5
Jean-Francois Robin <jean-francois_robin@uqar.ca>
2023-04-21 10:04:02 -0400

    fixed teleport information

--- Runtime/BaseClasses/TeleportInformation.cs
+++ Runtime/BaseClasses/TeleportInformation.cs
@@ -10,22 +10,19 @@ namespace jeanf.EventSystem
         public bool objectIsPlayer;
         public Transform objectToTeleport;
         public Transform targetDestination;
-        public bool alignToTarget;
         public bool isUsingFilter;
         public FilterSO filter;
         
         public TeleportInformation(Transform targetDestination, bool alignToTarget)
         {
             this.targetDestination = targetDestination;
-            this.alignToTarget = alignToTarget;
             this.objectIsPlayer = true;
         }
 		
-        public TeleportInformation(Transform objectToTeleport, Transform targetDestination, bool alignToTarget, bool objectIsPlayer, FilterSO filter, bool isUsingFilter)
+        public TeleportInformation(Transform objectToTeleport, Transform targetDestination, bool objectIsPlayer, FilterSO filter, bool isUsingFilter)
         {
             this.objectToTeleport = objectToTeleport;
             this.targetDestination = targetDestination;
-            this.alignToTarget = alignToTarget;
             this.objectIsPlayer = objectIsPlayer;
             this.filter = filter;
             this.isUsingFilter = isUsingFilter;
