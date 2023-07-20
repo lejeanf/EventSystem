14db44b11c09f19849f49f48095ae574ec5f96e6
Jean-Francois Robin <jean-francois_robin@uqar.ca>
2023-07-20 12:53:45 -0400

    added float event type

---
+++ Runtime/SenderInterfaces/FloatEventSender.cs
@@ -0,0 +1,19 @@
+using jeanf.EventSystem;
+using UnityEngine;
+
+public class FloatEventSender : MonoBehaviour, IDebugBehaviour
+{
+    public bool isDebug
+    { 
+        get => _isDebug;
+        set => _isDebug = value; 
+    }
+    [SerializeField] private bool _isDebug = false;
+
+    [field: Header("Broadcasting on:")] public FloatEventChannelSO floatMessageChannel;
+    
+    public void SendFloat(float value)
+    {
+        floatMessageChannel.RaiseEvent(value);
+    }
+}
