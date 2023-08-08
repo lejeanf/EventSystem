dab3547cc1a35a28ed2ef047c8e5ad9506576afa
Jean-Francois Robin <jean-francois_robin@uqar.ca>
2023-08-08 12:07:40 -0400

    added int-bool event for testing

---
+++ Runtime/Listeners/IntBoolEventListener.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// To use a generic UnityEvent type you must override the generic type.
+/// </summary>
+namespace jeanf.EventSystem
+{
+	[System.Serializable]
+	public class IntBoolEvent : UnityEvent<int, bool>
+	{
+
+	}
+
+	/// <summary>
+	/// A flexible handler for int events in the form of a MonoBehaviour. Responses can be connected directly from the Unity Inspector.
+	/// </summary>
+	public class IntBoolEventListener : MonoBehaviour, IDebugBehaviour
+	{
+		public bool isDebug
+		{ 
+		    get => _isDebug;
+		    set => _isDebug = value; 
+		}
+		[SerializeField] private bool _isDebug = false;
+		
+		[SerializeField] private IntBoolEventChannelSO _channel = default;
+
+		public IntBoolEvent OnEventRaised;
+
+		private void OnEnable()
+		{
+			if (_channel != null)
+				_channel.OnEventRaised += Respond;
+		}
+
+		private void OnDisable()
+		{
+			if (_channel != null)
+				_channel.OnEventRaised -= Respond;
+		}
+
+		private void Respond(int nb, bool value)
+		{
+			OnEventRaised?.Invoke(nb, value);
+			if(isDebug) Debug.Log($" int-bool event raised: <{nb},{value}>");
+		}
+	}
+}
---
+++ Runtime/SenderInterfaces/IntBoolEventSender.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace jeanf.EventSystem
+{
+    public class IntBoolEventSender : MonoBehaviour, IDebugBehaviour
+    {
+        public bool isDebug
+        { 
+            get => _isDebug;
+            set => _isDebug = value; 
+        }
+        [SerializeField] private bool _isDebug = false;
+
+        [field: Header("Broadcasting on:")] public IntBoolEventChannelSO boolMessageChannel;
+    
+        public void SendBool(int nb, bool value)
+        {
+            boolMessageChannel.RaiseEvent(nb, value);
+        }
+    }
+}
+
