1e3d5e516dd4aafee03797695632e9db3e2502f0
jeanf <robije02@uqar.ca>
2025-12-08 11:28:34 -0500

    Ènullref check

--- Runtime/SenderInterfaces/VoidEventSender.cs
+++ Runtime/SenderInterfaces/VoidEventSender.cs
@@ -22,6 +22,13 @@ namespace jeanf.EventSystem
 
         public void SendVoidEvent()
         {
+            if (voidMessageChannel == null)
+            {
+                Debug.LogError($"[VoidEventSender] Missing voidMessageChannel reference on GameObject: {gameObject.name}. " +
+                             $"Please assign a VoidEventChannelSO to this component.", this);
+                return;
+            }
+
             voidMessageChannel.RaiseEvent();
         }
     }
