8314235c0f3432fc781bb50f29894828bae95a49
Robin Jean-François <robije02@uqar.ca>
2026-02-18 11:10:20 -0500

    fix: added should fade parameter to teleport call to not use the fade during region change and avoid wierd behaviors

--- Runtime/BaseClasses/TeleportInformation.cs
+++ Runtime/BaseClasses/TeleportInformation.cs
@@ -9,6 +9,9 @@ namespace jeanf.EventSystem
         public Transform targetDestination;
         public bool isUsingFilter;
         public FilterSO filter;
+        public bool shouldFade; // screen fade in/out during teleport
+        
+        // fade is activated by default if we don't specify otherwise
         public TeleportInformation(Transform objectToTeleport, Transform targetDestination, bool objectIsPlayer, FilterSO filter, bool isUsingFilter)
         {
             this.objectToTeleport = objectToTeleport;
@@ -16,6 +19,18 @@ namespace jeanf.EventSystem
             this.objectIsPlayer = objectIsPlayer;
             this.filter = filter;
             this.isUsingFilter = isUsingFilter;
+            this.shouldFade = true;
+        }
+        
+        // here we can specify to desactivate the fade using the shouldFade parameter
+        public TeleportInformation(Transform objectToTeleport, Transform targetDestination, bool objectIsPlayer, FilterSO filter, bool isUsingFilter,  bool shouldFade)
+        {
+            this.objectToTeleport = objectToTeleport;
+            this.targetDestination = targetDestination;
+            this.objectIsPlayer = objectIsPlayer;
+            this.filter = filter;
+            this.isUsingFilter = isUsingFilter;
+            this.shouldFade = shouldFade;
         }
     }
 }
